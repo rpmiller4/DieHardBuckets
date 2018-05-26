77b0cce9786c4c66275ed7f86b404ddc0cc96233
Robert Miller <rpmiller4@gmail.com>
2018-05-25 18:53:36 -0700

    Create Bucket class and an interface for Bucket manipulations

---
+++ BucketProblems/BucketProblems/Bucket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketProblems
+{
+    /// <summary>
+    /// Likely don't need an entire bucket class for 2-bucket problems, but demonstrates OOP.
+    /// </summary>
+    public class Bucket
+    {
+        private int BucketSize { get; set; }
+        public int Volume { get; set; }
+
+        public Bucket(int bucketSize)
+        {
+            BucketSize = bucketSize;
+        }
+    }
+}
---
+++ BucketProblems/BucketProblems/ISolveBucketProblems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketProblems
+{
+    //Bucket Operations
+    interface IManipulateBuckets
+    {
+        void FillBucket(Bucket bucket);
+        void EmptyBucket(Bucket bucket);
+        void Transfer(Bucket source, Bucket target);
+        void Transfer(Bucket source, Bucket target, int volume);
+    }
+}
